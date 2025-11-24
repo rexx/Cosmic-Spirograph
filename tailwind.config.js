0cc9ce635bf53503c23f5cd2cff8f3f94979aec2
gtso <gene.tso@yahooinc.com>
2025-11-24 22:55:18 +0800

    Refactor: Migrate to Vite/React/TS build system with GitHub Actions

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          850: '#1f2937',
+          950: '#030712',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
